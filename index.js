fdb68e324cbaefb84631f5b4409017ce95d23a06
AbhiVarde <91776428+AbhiVarde@users.noreply.github.com>
2022-07-14 15:27:28 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// select humburger 
+const hamburger = document.querySelector('.hamburger')
+// select nav-menu
+const navMenu = document.querySelector('.nav-menu')
+// add event listener
+hamburger.addEventListener('click', mobileMenu)
+// navmenu function
+    //toggle active
+    //toggle active
+function mobileMenu() {
+    hamburger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+}
